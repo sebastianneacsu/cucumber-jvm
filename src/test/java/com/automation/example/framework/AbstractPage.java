0c0a35d189e08539b1fde274bef40aace9e55ebf
emneacsse <sebastian.neacsu@arhs-developments.com>
2018-12-28 10:44:30 +0100

    added environment property file and method to get the values from it

--- src/test/java/com/automation/example/framework/AbstractPage.java
+++ src/test/java/com/automation/example/framework/AbstractPage.java
@@ -19,4 +19,49 @@ public abstract class AbstractPage {
         return new WebDriverWait(driver, timeoutSeconds);
     }
 
+ public String getEnvironment() {
+        return "local";
+    }
+
+    public HashMap <String, String> getEnvProps(){
+        HashMap<String, String> hmap = new HashMap();
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("src\\test\\resources\\data\\environment.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            if (this.getEnvironment() == "local"){
+                hmap.put("username", prop.getProperty("localUsername"));
+                hmap.put("url", prop.getProperty("localUrl"));
+                hmap.put("password", prop.getProperty("localPassword"));
+            } else if (this.getEnvironment() == "test"){
+                hmap.put("username", prop.getProperty("testUsername"));
+                hmap.put("url", prop.getProperty("testUrl"));
+                hmap.put("password", prop.getProperty("testPassword"));
+            }
+            else if (this.getEnvironment() == "acc"){
+                hmap.put("username", prop.getProperty("accUsername"));
+                hmap.put("url", prop.getProperty("accUrl"));
+                hmap.put("password", prop.getProperty("accPassword"));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return hmap;
+    }
+
 }
